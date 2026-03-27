c4e6fde3060a242e8914a4490c9de6e40cf914b1
Vitalii_Zlotnyk <vitalikzlot12@gmail.com>
2026-03-27 11:23:15 +0100

    Complete 4-th task

---
+++ js/task-4.js
@@ -0,0 +1,26 @@
+function getShippingCost(country) {
+  switch (country) {
+    case 'China':
+      var price = 100;
+      return `Shipping to ${country} will cost ${price} credits`;
+      s;
+    case 'Chile':
+      var price = 250;
+      return `Shipping to ${country} will cost ${price} credits`;
+    case 'Australia':
+      var price = 170;
+      return `Shipping to ${country} will cost ${price} credits`;
+    case 'Jamaica':
+      var price = 120;
+      return `Shipping to ${country} will cost ${price} credits`;
+    default:
+      return 'Sorry, there is no delivery to your country';
+  }
+}
+
+console.log(getShippingCost('Australia'));
+console.log(getShippingCost('Germany'));
+console.log(getShippingCost('China'));
+console.log(getShippingCost('Chile'));
+console.log(getShippingCost('Jamaica'));
+console.log(getShippingCost('Sweden'));
